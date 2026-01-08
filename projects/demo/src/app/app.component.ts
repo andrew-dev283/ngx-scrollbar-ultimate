99e582d222fc7356754a6e361513d3a1f223d574
Andrey Sokolov <a.sokolov_ext@yadro.com>
2026-01-08 22:28:28 +0300

    add demo

---
+++ projects/demo/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgxScrollbarUltimateComponent} from 'ngx-scrollbar-ultimate';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    NgxScrollbarUltimateComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+
+}
